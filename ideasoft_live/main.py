3b0f55ed182976aa3e9cf85bc523a6697bf3bf3a
Ahmetcan Güvendiren <ahm@glacion.com>
2020-04-14 21:06:28 +0300

    add CLI args

--- ideasoft_live/main.py
+++ ideasoft_live/main.py
@@ -2,32 +2,58 @@ import time
 
 import requests
 
-ENDPOINT = 'https://canli.ideasoft.com.tr/api/v1/live/export.php'
+# TODO: This will leak memory.
 items = []
-refreshed = False
 
 
-def decode(obj):
-    return f'{obj["city"]}/{obj["county"]}, {obj["price"]}\n{obj["quantity"]}, {obj["name"]}\n---'
+def parse(obj):
+    return f'{obj["city"]}, {obj["price"]}, {obj["quantity"]}\n\thttps:{obj["image"]}\n\t{obj["name"]}\n---'
 
 
-def print_items(response):
+def csv(sale):
+    return f'{sale["id"]},{sale["city"]},{sale["name"]},{sale["price"]},{sale["image"]},{sale["quantity"]},{sale["time"]}'
+
+
+def print_items(response, args):
     for sale in response['sales']:
         if sale['id'] not in items:
             items.append(sale['id'])
-            print(decode(sale))
+            if args.csv:
+                print(csv(sale))
+            else:
+                print(parse(sale))
 
 
-def perform():
+def perform(args):
+    params = {'refreshed': False}
     with requests.Session() as session:
         while True:
-            response = session.get(ENDPOINT).json()
-            print_items(response)
-            time.sleep(1)
+            response = session.get(args.endpoint).json()
+            print_items(response, args)
+            time.sleep(args.interval)
+
+
+def create_parser():
+    import argparse
+    parser = argparse.ArgumentParser(
+        prog='ideasoft_live', description='Polls sale data published by ideasoft.')
+    parser.add_argument(
+        '--csv', help='print sales as csv lines.', action='store_true')
+    parser.add_argument('--interval', default=1, type=int,
+                        help='time between the calls to server.')
+    parser.add_argument(
+        '--endpoint', default='https://canli.ideasoft.com.tr/api/v1/live/export.php', help='API endpoint to poll.')
+    return parser
 
 
 def main():
     try:
-        perform()
+        parser = create_parser()
+        args = parser.parse_args()
+        perform(args)
     except KeyboardInterrupt:
         print('\rExiting')
+
+
+if __name__ == '__main__':
+    main()
